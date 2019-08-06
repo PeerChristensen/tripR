9b6487231efad335c32413a099fecce892480838
PeerChristensen <hr.pchristensen@gmail.com>
2019-08-06 11:47:51 +0200

    add retry

--- R/main.R
+++ R/main.R
@@ -32,7 +32,7 @@ get_reviews <- function(base_url, page_lim = NULL, company = NULL, verbose = TRU
     }
 
     # get HTML
-    html <- url %>% xml2::read_html()
+    html <- url %>% retry(xml2::read_html())
 
     # get name
     if (str_detect(base_url,"Attraction")) {
@@ -98,3 +98,27 @@ get_reviews <- function(base_url, page_lim = NULL, company = NULL, verbose = TRU
 
 }
 
+# function for retries
+library(futile.logger)
+library(utils)
+
+retry <- function(expr, isError=function(x) "try-error" %in% class(x), maxErrors=5, sleep=0) {
+  attempts = 0
+  retval = try(eval(expr))
+  while (isError(retval)) {
+    attempts = attempts + 1
+    if (attempts >= maxErrors) {
+      msg = sprintf("retry: too many retries [[%s]]", capture.output(str(retval)))
+      flog.fatal(msg)
+      stop(msg)
+    } else {
+      msg = sprintf("retry: error in attempt %i/%i [[%s]]", attempts, maxErrors,
+                    capture.output(str(retval)))
+      flog.error(msg)
+      warning(msg)
+    }
+    if (sleep > 0) Sys.sleep(sleep)
+    retval = try(eval(expr))
+  }
+  return(retval)
+}
